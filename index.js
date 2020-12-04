11d2b7cd50740caa98d6f36e0f4b9aa99b232dce
Stefany-Souza <70451854+Stefany-Souza@users.noreply.github.com>
2020-12-04 19:50:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+async function csvField(url, campo){
+    let list = [];
+    let a ;
+     return await axios.get(url).then( function(response){   
+        let obj = $.csv.toObjects(response.data);
+        for(var i=0; i< obj.length;i++){
+            for(var aux in obj[i]){
+                if(aux == campo)
+                    list.push(obj[i][aux]);         
+            }
+        }
+                     
+    }).then(res => list);   
+}
+
+async function main (){
+    
+    const list1 = await csvField('https://raw.githubusercontent.com/UserZeca/Enxurrada-de-Bits-Exercicios/master/docs/Characters.csv', 'Character');
+    const list2 = await csvField('https://raw.githubusercontent.com/UserZeca/Enxurrada-de-Bits-Exercicios/master/docs/Characters.csv', 'Description/role');
+
+    for(let i = 0; i < list1.length; i++){
+        let lista = document.createElement('ul'); 
+        let elementoLista = document.createElement('li'); 
+        let conteudoElementoLista = document.createTextNode(list1[i]);
+       
+        elementoLista.appendChild(conteudoElementoLista);
+        lista.appendChild(elementoLista);
+       
+       let subLista = document.createElement('ul'); 
+       let elementoSubLista = document.createElement('li'); 
+       let conteudoElementoSubLista = document.createTextNode(list2[i]);
+       
+       elementoSubLista.appendChild(conteudoElementoSubLista);
+       subLista.appendChild(elementoSubLista);
+       lista.appendChild(subLista);
+    
+       document.getElementById('nomeDescricao').insertAdjacentElement('beforeend', lista);
+    
+       let estilo1 = document.createAttribute('class');
+       estilo1.value = "lista";
+       elementoLista.setAttributeNode(estilo1);
+    
+       let estilo2 = document.createAttribute('class');
+       estilo2.value = "sublista";
+       elementoSubLista.setAttributeNode(estilo2);
+    } 
+}
+
+
+window.onload = () => {
+
+    main()
+
+}
